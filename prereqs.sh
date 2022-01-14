1135a731cd5f6d147b5e632eb5970bfe26e2f65d
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2022-01-14 21:32:04 +0100

    Add pre-reqs for building

---
+++ prereqs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+sudo apt-get install \
+    liblxqt-globalkeys0-dev \
+    liblxqt-globalkeys-ui0-dev \
+    libqt5svg5-dev \
+    libkf5idletime-dev \
+    libkf5solid-dev \
+    liblxqt0-dev \
+    libqt5x11extras5-dev \
+    libkf5windowsystem-dev
